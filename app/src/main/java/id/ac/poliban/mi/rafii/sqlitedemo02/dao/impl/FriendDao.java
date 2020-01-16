8fec5804ff7a65184cfbdbe668830765aa6565ef
raffihidayat <rafizhe@gmail.com>
2020-01-17 00:45:18 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/rafii/sqlitedemo02/dao/impl/FriendDao.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.rafii.sqlitedemo02.dao.impl;
+
+
+import java.util.List;
+
+import id.ac.poliban.mi.rafii.sqlitedemo02.domain.Friend;
+
+public interface FriendDao {
+    void insert(Friend f);
+    void update(Friend f);
+    void delete(int id);
+    Friend getFriendById(int id);
+    List<Friend> getAllFriends();
+}
